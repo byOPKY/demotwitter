c26c49e75f4f7ff6198e3de5e1e19596a9fc1ed7
byOPKY <orrillokarol@gmail.com>
2016-09-19 20:21:00 -0500

    volumen 2

--- README.md
+++ README.md
@@ -1,6 +1,39 @@
-.-Tener un contenedor vacio que acumule los mensajes
+##Tener un contenedor vacio que acumule los mensajes##
 1. Formulario: TextArea y Botón
 2. Evento para el click el botón
 3. Dentro del evento, obtener el valor del textarea
 4. Darle formmato al texto del textArea
 5. Agregar el texto formateado al contenedor.
+
+##Desarrollo##
+1. Cree los doocumentos: 
+  * index.html
+  * main.css
+  * app.js
+  * simplegrip.css (lo busque y lo modifique)
+2. Enlace los documentos.
+3. En el documento main.css hice:
+  * Una funcion global window.onload
+  * Cree un evento addEventListener - click, para el boton
+  * Cree la variable: area; para llamar al textArea (document.getElementById) y para coger el valor le puse: .value
+  * Cree la variable: demo; para llamar al ID demo  (document.getElementById)
+  * Cree una variable msj con document.createElement: div
+  * Use el inner.HTML con el msj para igualarlo con el valor del area 
+  * Le agregue una clase "cuadro" al msj con .classList.add("")
+  * Para incrustar el msj use el inserBefote() y como quiero que aparezca primero uso el child.Nodes[0] con indice 0 para que salga antes del primer hijo.
+  * Cree una variable contador con createElement para crear un div
+  * Iguale la cantidad de caracteres del area (area.length) con lo que se va escribir en el Html del contador (contador.inner.HTML)
+  * Para incrustar la cantidad de caracteres use el demo.insetBefore(ontador, msj) siendo el demo el padre.
+
+1. No ingresar texto vacio (deshabilitar el boton de enviar)
+2. Contar la cantidad de caracteres de forma regresiva.
+-- v0.0.2
+
+3. Si pasa los 140 caracteres, deshabilitar el botón.
+4. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+5. Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+6. si pasa los 140 caracteres, mostrar el contador en negativo.
+
+-- v0.0.3
+7. Agrega el tweet con un "enter" y click
+Coming soon.
--- js/app.js
+++ js/app.js
@@ -1,21 +1,91 @@
+window.onload = function () {
+	var textArea= document.getElementById("textArea");
+	var boton = document.getElementById("boton");
+	var contador =document.getElementById("contador");
+	contador.innerHTML = 140;
+	contador.classList.add("cuadro2");
+	boton.disabled = true ;
 
+	boton.addEventListener("click", function(e) {
+		e.preventDefault();
+	    var area = textArea.value;
+	    var demo = document.getElementById("demo");
+	   	var msj = document.createElement("div");
+	   	msj.innerHTML = area;
+	   	msj.classList.add("cuadro");
+	   	demo.insertBefore(msj, demo.childNodes[0]);
+	   	
+	   	textArea.value= "";
+	   	contador.innerHTML = 140;
+	});
+
+	textArea.addEventListener("keydown",function() {
+		boton.disabled = false;
+			if (textArea.value.length>=140){
+				contador.style.color = "red";
+				contador.innerHTML = 140 - textArea.value.length; 
+			}
+			else{
+				contador.innerHTML= 140 - textArea.value.length;	
+			}
+			if(textArea.value.length==0){
+				boton.disabled = true;
+			}
+
+	});	
+
+}
+
+/*
 window.onload = function () {
 	var boton = document.getElementById("boton");
-	boton.addEventListener("click", function() {
-	    var area = document.getElementById("textArea").value;
+	boton.addEventListener("click", function(e) {
+		
+		e.preventDefault();
+		var textArea= document.getElementById("textArea")
+	    var area = textArea.value;
+	    agregarMensaje(textArea.value);
+	   	textArea.value= "";
+	   	 function agregarMensaje(textArea){
+	   	 	var msj = document.createElement("div");
+	   		msj.innerHTML = area;
+	   		var demo = document.getElementById("demo");
+	   		msj.classList.add("cuadro");
+	   		demo.insertBefore(msj, demo.childNodes[0])
+
+	   		var contador = document.createElement("div");
+		   	contador.innerHTML= area.length;
+		   	contador.classList.add("cuadro2");
+		   	demo.insertBefore(contador, msj);
+	   	 }
+	});
+}
+
+*/
+
+/*
+window.onload = function () {
+	var boton = document.getElementById("boton");
+	boton.addEventListener("click", function(e) {
+		
+		e.preventDefault();
+		var textArea= document.getElementById("textArea")
+	    var area = textArea.value;
 	    var demo = document.getElementById("demo");
 	   	var msj = document.createElement("div");
 	   	msj.innerHTML = area;
 	   	msj.classList.add("cuadro");
 	   	demo.insertBefore(msj, demo.childNodes[0])
+	   	
+	   	textArea.value= ""
+
 	   	var contador = document.createElement("div");
 	   	contador.innerHTML= area.length;
 	   	contador.classList.add("cuadro2");
 	   	demo.insertBefore(contador, msj);
 	});
 }
-
-
+*/
 /*window.onload = function () {
 	var boton = document.getElementById("boton");
 	boton.addEventListener("click", function() {
