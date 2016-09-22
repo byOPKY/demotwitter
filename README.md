82b9a69985b86a5b877dde8a463bb875fef6ea89
byOPKY <orrillokarol@gmail.com>
2016-09-22 08:57:34 -0500

    subiendo

--- README.md
+++ README.md
@@ -3,4 +3,50 @@
 2. Evento para el click el botón
 3. Dentro del evento, obtener el valor del textarea
 4. Darle formmato al texto del textArea
+<<<<<<< gh-pages
 5. Agregar el texto formateado al contenedor.
+=======
+5. Agregar el texto formateado al contenedor.
+
+##Desarrollo##
+1. Cree los doocumentos: 
+  * index.html
+  * main.css
+  * app.js
+  * simplegrip.css (lo busque y lo modifique)
+2. Enlace los documentos.
+3. En el documento main.css hice:
+  * Una funcion global window.onload
+  * Cree un evento addEventListener - click, para el boton
+  * Cree la variable: area; para llamar al textArea (document.getElementById) y para coger el valor le puse: .value
+  * Cree la variable: demo; para llamar al ID demo  (document.getElementById)
+  * Cree una variable msj con document.createElement: div
+  * Use el inner.HTML con el msj para igualarlo con el valor del area 
+  * Le agregue una clase "cuadro" al msj con .classList.add("")
+  * Para incrustar el msj use el inserBefote() y como quiero que aparezca primero uso el child.Nodes[0] con indice 0 para que salga antes del primer hijo.
+  * Cree una variable contador con createElement para crear un div
+  * Iguale la cantidad de caracteres del area (area.length) con lo que se va escribir en el Html del contador (contador.inner.HTML)
+  * Para incrustar la cantidad de caracteres use el demo.insetBefore(ontador, msj) siendo el demo el padre.
+
+1. No ingresar texto vacio (deshabilitar el boton de enviar)
+2. Contar la cantidad de caracteres de forma regresiva.
+-- v0.0.2
+
+3. Si pasa los 140 caracteres, deshabilitar el botón.
+4. Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+5. Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+6. si pasa los 140 caracteres, mostrar el contador en negativo.
+
+-- v0.0.3
+7. Agrega el tweet con un "enter" y click
+Al presionar enter ("/n") que crezca el textarea de acuerdo al tamaño del texo.
+-- v0.0.4
+8. Si la cantidad de caracteres ingresados (sin dar un enter), supera al tamaño del textarea por defecto, ebe de agregarse una linea mas para que no parezca el scrooll. (si en caso aplica)
+-- v0.0.6
+9. Agregar la hora en que se publico el tweet.
+
+
+Git Tagging:
+git tag -a v0.0.x -m "Version v0.0.x"
+git push origin v0.0.x
+>>>>>>> local
--- js/app.js
+++ js/app.js
@@ -1,4 +1,131 @@
+<<<<<<< gh-pages
 
+=======
+window.onload = function () {
+	var textArea= document.getElementById("textArea");
+	var boton = document.getElementById("boton");
+	var contador =document.getElementById("contador");
+	contador.innerHTML = 140;
+	contador.classList.add("cuadro2");
+	boton.disabled = true ;
+
+	boton.addEventListener("click", function(e) {
+		e.preventDefault();
+	    var area = textArea.value;
+	    var demo = document.getElementById("demo");
+	   	var msj = document.createElement("div");
+	   	msj.innerHTML = area;
+	   	msj.classList.add("cuadro");
+	   	msj.classList.add("col-7");
+	   	demo.insertBefore(msj, demo.childNodes[0]);
+	   	
+	   	textArea.value= "";
+	   	contador.innerHTML = 140;
+
+	   	var fechaHora = new Date();
+		var horas = fechaHora.getHours();
+		var minutos = fechaHora.getMinutes();
+		var segundos = fechaHora.getSeconds();
+		var reloj = document.createElement("div");
+		if(horas < 10) { 
+			horas = '0' + horas; }
+  		if(minutos < 10) { 
+  			minutos = '0' + minutos; }
+  		if(segundos < 10) { 
+  			segundos = '0' + segundos; }
+		reloj.innerHTML = horas+':'+minutos+':'+segundos;
+		reloj.classList.add("col-4");
+		reloj.classList.add("cuadro3");
+	   	demo.insertBefore(reloj, demo.childNodes[1]);
+
+	});
+
+	textArea.addEventListener("keydown",function() {
+		boton.disabled = false;
+			if (textArea.value.length>=140){
+				contador.style.color = "red";
+				boton.disabled = true;
+				contador.innerHTML = 140 - textArea.value.length; 
+			} 
+			else if (textArea.value.length>=130){
+				contador.style.color = "blue";
+				contador.innerHTML = 140 - textArea.value.length; 
+			}
+			else if (textArea.value.length>=120){
+				contador.style.color = "purple";
+				contador.innerHTML = 140 - textArea.value.length; 
+			}
+			else if (textArea.value.length>=110){
+				contador.style.color = "black";
+				contador.innerHTML = 140 - textArea.value.length; 
+			}
+			else{
+				contador.innerHTML= 140 - textArea.value.length;	
+			}
+			//Inhabilita el boton en espacios vacios 
+			if( textArea.value == null || textArea.value.length == 0 || /^\s+$/.test(textArea.value) ) {
+ 				boton.disabled = true;
+				} 
+	});
+
+		textArea.addEventListener("keydown", autosize);        
+		function autosize(){
+		 	 var el = this; 
+		 	el.style.cssText = "padding:1; height:auto";
+		 	el.style.cssText = "height:" + el.scrollHeight + "px";
+   		};
+    }
+/*
+ar textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+             
+function autosize(){
+  var el = this;
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'padding:0; height:' + el.scrollHeight + 'px';
+}
+
+textArea.addEventListener("scroll", function(){
+		//textArea.setAttribute("rows","textArea.scrollHeight");
+		textArea.style.height = textArea.scrollHeight + "px";
+	});
+
+textArea.addEventListener("change", function(){
+		//textArea.setAttribute("rows","textArea.scrollHeight");
+		textArea.style.height = 2;
+	});
+
+
+window.onload = function () {
+	var boton = document.getElementById("boton");
+	boton.addEventListener("click", function(e) {
+		
+		e.preventDefault();
+		var textArea= document.getElementById("textArea")
+	    var area = textArea.value;
+	    agregarMensaje(textArea.value);
+	   	textArea.value= "";
+	   	 function agregarMensaje(textArea){
+	   	 	var msj = document.createElement("div");
+	   		msj.innerHTML = area;
+	   		var demo = document.getElementById("demo");
+	   		msj.classList.add("cuadro");
+	   		demo.insertBefore(msj, demo.childNodes[0])
+
+	   		var contador = document.createElement("div");
+		   	contador.innerHTML= area.length;
+		   	contador.classList.add("cuadro2");
+		   	demo.insertBefore(contador, msj);
+	   	 }
+	});
+}
+
+*/
+
+/*
+>>>>>>> local
 window.onload = function () {
 	var boton = document.getElementById("boton");
 	boton.addEventListener("click", function() {
